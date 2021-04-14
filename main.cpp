dd2d27e339a14d922fa30b34383ba14648ba34f4
Eragon35 <antipin35@yahoo.com>
2021-04-14 11:16:40 +0300

    Exclude check method & change output to file

--- main.cpp
+++ main.cpp
@@ -1,86 +1,89 @@
 #include <iostream>
 #include <windows.h>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
-int main(int argc, const char* argv[]) {
-//    ofstream output ("output.txt");
-//    if(argc != 2) {
-//        cout << "Usage: sectons.exe pe_file" << endl;
-//        return 0;
-//    }
-    ifstream input;
-    input.open("SpotifySetup.exe", ios::in | ios::binary); // TODO: change to argv[1]
+ifstream input;
+IMAGE_DOS_HEADER dos_header;
+
+bool checker() {
     if(!input.is_open()) { // если вдруг его открыть не удалось, то выведем ошибку и выйдем
         cout << "Can't open file" << endl;
-        return 0;
+        return true;
     }
-    input.seekg(0, ios::end); //для этого переведем файловый указатель чтения в самый конец файла, получим его позицию
-    streamoff filesize = input.tellg(); //это и будет размер файла в байтах
-    input.seekg(0); //затем вернем файловый указатель в начало файла
-
-    IMAGE_DOS_HEADER dos_header;
-//    TODO: exclude check for specials methods
     input.read(reinterpret_cast<char*>(&dos_header), sizeof(IMAGE_DOS_HEADER));
-    if(input.bad() || input.eof()) { //если вдруг считать не удалось...
+    if(input.bad() || input.eof()) { // если вдруг считать не удалось
         cout << "Unable to read IMAGE_DOS_HEADER" << endl;
-        return 0;
+        return true;
     }
-    if(dos_header.e_magic != 'ZM') { //Первые два байта структуры должны быть MZ, но, так как в
-        cout << "IMAGE_DOS_HEADER signature is incorrect" << endl; // x86 у нас обратный
-        return 0; // порядок следования байтов, мы сравниваем эти байты со значением 'ZM'
-    }
-    if((dos_header.e_lfanew % sizeof(DWORD)) != 0) { // Начало заголовка самого PE-файла (IMAGE_NT_HEADERS) должно быть
-        cout << "PE header is not DWORD-aligned" << endl; // выровнено на величину двойного слова (DWORD)
-        return 0; // а иначе наш PE-файл некорректен
-    }
-
     input.seekg(dos_header.e_lfanew); //Переходим на структуру IMAGE_NT_HEADERS и готовимся считать ее
     if(input.bad() || input.fail()) {
         cout << "Cannot reach IMAGE_NT_HEADERS" << endl;
-        return 0;
+        return true;
     }
+    if(dos_header.e_magic != 'ZM') { // Первые два байта структуры должны быть MZ, но в
+        cout << "IMAGE_DOS_HEADER signature is incorrect" << endl; // x86 обратный порядок
+        return true; // следования байтов, мы сравниваем эти байты со значением 'ZM'
+    }
+    if((dos_header.e_lfanew % sizeof(DWORD)) != 0) { // Начало заголовка самого PE-файла (IMAGE_NT_HEADERS)
+        cout << "PE header is not DWORD-aligned" << endl; // должно быть выровнено на величину DWORD
+        return true; // иначе наш PE-файл некорректен
+    }
+    return false;
+}
 
+int main() {
+    vector<IMAGE_SECTION_HEADER> vector;
 
-    IMAGE_NT_HEADERS nt_header; //читать будем IMAGE_NT_HEADERS только без дата директорий
+    input.open("SpotifySetup.exe", ios::in | ios::binary);
+    ofstream output ("output.txt");
+    ofstream bin ("bin.txt");
+    if (checker()) return 0;
+
+    IMAGE_NT_HEADERS nt_header; // читать будем IMAGE_NT_HEADERS только без дата директорий
     input.read(reinterpret_cast<char*>(&nt_header), sizeof(IMAGE_NT_HEADERS) - sizeof(IMAGE_DATA_DIRECTORY) * 16);
     if(input.bad() || input.eof()) {
         cout << "Error reading IMAGE_NT_HEADERS32" << endl;
         return 0;
     }
-    if(nt_header.Signature != 'EP') { //Проверяем, что наш файл - PE сигнатура у него должна быть
-        cout << "Incorrect PE signature" << endl; // "PE\0\0", EP из-за обратного порядок байтов
+    if(nt_header.Signature != 'EP') { // Проверяем, что наш файл - PE сигнатура
+        cout << "Incorrect PE signature" << endl;
         return 0;
     }
-    //позиция в файле таблицы секций - это размер всех заголовков полностью
+    // позиция в файле таблицы секций - это размер всех заголовков полностью
     DWORD first_section = dos_header.e_lfanew + nt_header.FileHeader.SizeOfOptionalHeader
             + sizeof(IMAGE_FILE_HEADER) + sizeof(DWORD);
 
-    input.seekg(first_section); //переходим на первую секцию в таблице секций
+    input.seekg(first_section); // переходим на первую секцию в таблице секций
     if(input.bad() || input.fail()) {
         std::cout << "Cannot reach section headers" << std::endl;
         return 0;
     }
     WORD number_of_sections = nt_header.FileHeader.NumberOfSections;
 
-    // TODO: Write to file instead of console
-    cout << "AddressOfEntryPoint: " << nt_header.OptionalHeader.AddressOfEntryPoint << endl; // адресе точки входа
+    output << "AddressOfEntryPoint: " << nt_header.OptionalHeader.AddressOfEntryPoint << endl; // адрес точки входа
 
-    for (WORD i = 0; i < number_of_sections; i++) { // секциях
+    for (WORD i = 0; i < number_of_sections; i++) { // выводим информацию о секциях
         IMAGE_SECTION_HEADER section_header;
         input.read(reinterpret_cast<char*>(&section_header), sizeof(IMAGE_SECTION_HEADER));
-        cout << "Name: " << section_header.Name << endl;
-        cout << "VirtualAddress: " << section_header.VirtualAddress << endl;
-        cout << "Characteristics: " << section_header.Characteristics << endl;
-        cout << "Misc.PhysicalAddress: " << section_header.Misc.PhysicalAddress << endl;
-        cout << "Misc.VirtualSize: "<< section_header.Misc.VirtualSize << endl;
-        cout << "NumberOfRelocations: "<< section_header.NumberOfRelocations << endl;
-        cout << "PointerToLinenumbers: "<< section_header.PointerToLinenumbers << endl;
-        cout << "PointerToRawData: "<< section_header.PointerToRawData << endl;
-        cout << "PointerToRelocations: "<< section_header.PointerToRelocations << endl;
-        cout << "SizeOfRawData: "<< section_header.SizeOfRawData << endl;
-        cout << "--------------------" << endl;
+        output << "Name: " << section_header.Name << endl;
+        output << "VirtualAddress: 0x" << hex << section_header.VirtualAddress << endl;
+        output << "Characteristics: 0x" << hex << section_header.Characteristics << endl;
+        output << "Misc.PhysicalAddress: 0x" << hex << section_header.Misc.PhysicalAddress << endl;
+        output << "Misc.VirtualSize: 0x" << hex << section_header.Misc.VirtualSize << endl;
+        output << "NumberOfRelocations: 0x" << hex << section_header.NumberOfRelocations << endl;
+        output << "PointerToLinenumbers: 0x" << hex << section_header.PointerToLinenumbers << endl;
+        output << "PointerToRawData: 0x" << hex << section_header.PointerToRawData << endl;
+        output << "PointerToRelocations: 0x" << hex << section_header.PointerToRelocations << endl;
+        output << "SizeOfRawData: 0x" << hex << section_header.SizeOfRawData << endl;
+        output << "--------------------" << endl;
+        if (section_header.Characteristics & 0x20) vector.push_back(section_header);
+    }
+
+    for (IMAGE_SECTION_HEADER header : vector) {
+        // TODO: write_binary_code
     }
     return 0;
 }
