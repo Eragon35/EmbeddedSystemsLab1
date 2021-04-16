3bb022697fcd459231de89f3be70ea385c3e6929
Eragon35 <antipin35@yahoo.com>
2021-04-17 01:19:27 +0300

    Finish version

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <windows.h>
 #include <fstream>
-#include <stdio.h>
+#include <cstdio>
 
 using namespace std;
 
@@ -11,44 +11,39 @@ IMAGE_NT_HEADERS nt_header; // читать будем IMAGE_NT_HEADERS толь
 
 
 bool checker() {
-    if(!input.is_open()) { // если вдруг его открыть не удалось, то выведем ошибку и выйдем
+    if(!input.is_open()) { // если файл не удалось открыть
         cout << "Can't open file" << endl;
         return true;
     }
     input.read(reinterpret_cast<char*>(&dos_header), sizeof(IMAGE_DOS_HEADER));
-    if(input.bad() || input.eof()) { // если вдруг считать не удалось
+    if(input.bad() || input.eof()) { // если не удалось считать
         cout << "Unable to read IMAGE_DOS_HEADER" << endl;
         return true;
     }
-    input.seekg(dos_header.e_lfanew); //Переходим на структуру IMAGE_NT_HEADERS и готовимся считать ее
+    input.seekg(dos_header.e_lfanew); // переходим на структуру IMAGE_NT_HEADERS
     if(input.bad() || input.fail()) {
         cout << "Cannot reach IMAGE_NT_HEADERS" << endl;
         return true;
     }
-    if(dos_header.e_magic != 'ZM') { // Первые два байта структуры должны быть MZ, но в
+    if(dos_header.e_magic != 'ZM') { // первые два байта структуры должны быть MZ, но в
         cout << "IMAGE_DOS_HEADER signature is incorrect" << endl; // x86 обратный порядок
         return true; // следования байтов, мы сравниваем эти байты со значением 'ZM'
     }
-    if((dos_header.e_lfanew % sizeof(DWORD)) != 0) { // Начало заголовка самого PE-файла (IMAGE_NT_HEADERS)
+    if((dos_header.e_lfanew % sizeof(DWORD)) != 0) { // начало заголовка самого PE-файла (IMAGE_NT_HEADERS)
         cout << "PE header is not DWORD-aligned" << endl; // должно быть выровнено на величину DWORD
         return true; // иначе наш PE-файл некорректен
     }
-    return false;
-}
-
-bool checker2(){
+    input.read(reinterpret_cast<char*>(&nt_header), sizeof(IMAGE_NT_HEADERS) - sizeof(IMAGE_DATA_DIRECTORY) * 16);
     if(input.bad() || input.eof()) {
         cout << "Error reading IMAGE_NT_HEADERS32" << endl;
         return true;
     }
-    if(nt_header.Signature != 'EP') { // Проверяем, что наш файл - PE сигнатура
+    if(nt_header.Signature != 'EP') { // проверяем, что наш файл - PE сигнатура
         cout << "Incorrect PE signature" << endl;
         return true;
     }
-    // позиция в файле таблицы секций - это размер всех заголовков полностью
     DWORD first_section = dos_header.e_lfanew + nt_header.FileHeader.SizeOfOptionalHeader
                           + sizeof(IMAGE_FILE_HEADER) + sizeof(DWORD);
-
     input.seekg(first_section); // переходим на первую секцию в таблице секций
     if(input.bad() || input.fail()) {
         std::cout << "Cannot reach section headers" << std::endl;
@@ -57,22 +52,24 @@ bool checker2(){
     return false;
 }
 
+
 int main() {
-    string inputFile = "SpotifySetup.exe";
-    string outputFile = "output.txt";
-    string binFile = "bin.txt";
+    const char *inputFile = "SpotifySetup.exe";
+    const char *outputFile = "output.txt";
+    const char *binFile = "bin.txt";
+//    FILE *binFile = fopen("bin.txt", "wb");
+//    FILE *file = fopen(inputFile, "rb");
     input.open(inputFile, ios::in | ios::binary);
     ofstream output (outputFile);
     ofstream bin (binFile);
+
     if (checker()) return 0;
 
-    input.read(reinterpret_cast<char*>(&nt_header), sizeof(IMAGE_NT_HEADERS) - sizeof(IMAGE_DATA_DIRECTORY) * 16);
-    if (checker2()) return 0;
     WORD number_of_sections = nt_header.FileHeader.NumberOfSections;
+    DWORD pointerToCode;
+    DWORD sizeOfCode;
 
     output << "AddressOfEntryPoint: " << nt_header.OptionalHeader.AddressOfEntryPoint << endl; // адрес точки входа
-
-    DWORD pointerToCode, sizeOfCode;
 
     for (WORD i = 0; i < number_of_sections; i++) { // выводим информацию о секциях
         IMAGE_SECTION_HEADER section_header;
@@ -88,12 +85,18 @@ int main() {
         output << "PointerToRelocations: 0x" << hex << section_header.PointerToRelocations << endl;
         output << "SizeOfRawData: 0x" << hex << section_header.SizeOfRawData << endl;
         output << "--------------------" << endl;
-        if (section_header.Characteristics & 0x20) {
-            pointerToCode = section_header.PointerToRawData;
-            sizeOfCode = section_header.SizeOfRawData;
-//            save pointerToCode & sizeOfCode
+        if (section_header.Characteristics & 0x20) { // если этот заголовок .text, то сохраняем его
+            pointerToCode = section_header.PointerToRawData; // начало
+            sizeOfCode = section_header.SizeOfRawData; // и продолжительность
         }
     }
+//    fseek(file, pointerToCode, SEEK_SET); // ищем начало секции с кодом
+    input.seekg(pointerToCode);
+    char* binCode = new char[sizeOfCode];
+    input.read(binCode, sizeOfCode); // считываем в binCode бинарынй код
+    bin.write(binCode, sizeOfCode); // записываем бинарный код
+//    fread(binCode, sizeof(std::byte), sizeOfCode, file);
+//    fwrite(binCode, sizeof(std::byte), sizeOfCode, binFile);
 
     return 0;
 }
